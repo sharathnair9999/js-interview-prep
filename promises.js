fd52edbeabb8840f56e440bdd8872ed6505ba97e
Sharath <sharathnair9999@gmail.com>
2022-07-02 14:54:51 +0530

    promises

---
+++ promises.js
@@ -0,0 +1,31 @@
+const divisbleBy2Promise = (num) =>
+  new Promise((resolve, reject) => {
+    if (num % 2 === 0) {
+      setTimeout(() => {
+        resolve("Number divisible by 2");
+      }, 2000);
+    } else {
+      reject(new Error("That is not divisible by 2"));
+    }
+  });
+const primePromise = (num) =>
+  new Promise((resolve, reject) => {
+    let bool = true;
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        bool = false;
+        break;
+      }
+    }
+    if (bool) {
+      setTimeout(() => {
+        resolve("Number is Prime");
+      }, 1000);
+    } else {
+      reject(new Error("Not a prime"));
+    }
+  });
+
+Promise.race([divisbleBy2Promise(2), primePromise(5)]).then((values) =>
+  console.log(values)
+);
